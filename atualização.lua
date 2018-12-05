cc1c029e18da34e396f1d9dcf55d9ac7a07c48f9
Gabriel Ribeiro <xxhawker123@gmail.com>
2018-12-05 18:57:50 -0200

    Atualização.lua
    [Atualização] : 2/12/2018 As 16:37
    
    1 - Adicionado LOADSCREEN!
    [Atualização] : 2/12/2018 As 16:37
    
    1 - Adicionado ANUNCIOS!
    Gabriel&MilenaÚltimo Domingo às 17:02
    [Atualização] : 2/12/2018 As 17:02
    1 - Adicionado Customchat
    2 - Cinto de segurança
    3 - /dv
    [Atualização] : 2/12/2018 As 17:02
    1 - Adicionado sem mapa!
    Gabriel&MilenaÚltimo Domingo às 17:19
    [Atualização] : 2/12/2018 As 17:02
    1 - Adicionado k9
    [Atualização] : 2/12/2018 As 17:02
    1 - Adicionado fora do rp
    [Atualização] : 2/12/2018 As 17:02
    1 - Adicionado anonimo
    [Atualização] : 2/12/2018 As 17:02
    1 - Adicionado twitter
    Gabriel&MilenaÚltimo Domingo às 17:31
    [Atualização] : 2/12/2018 As 17:31
    1 - Adicionado Menu admin v2.0(editado)
    [Atualização] : 2/12/2018 As 17:31
    1 - Adicionado topo com nome do servidor e discord
    [Atualização] : 2/12/2018 As 17:31
    1 - Adicionado voz no canto direito inferior
    [Atualização] : 2/12/2018 As 17:31
    1 - Adicionado Garage 2.0
    Gabriel&MilenaÚltimo Domingo às 17:41
    [Atualização] : 2/12/2018 As 17:31
    1 - Adicionado correção do celular
    Gabriel&MilenaÚltimo Domingo às 18:26
    [Atualização] : 2/12/2018 As 17:31
    1 - Adicionado nome do servidor
    LILITH ADVERTIDAÚltimo Domingo às 19:20
    Gabriel, deixa a tela mais clean, o nome do server, só vai poluir a tela
    Gabriel&MilenaÚltimo Domingo às 19:57
    [Atualização] : 2/12/2018 As 19:56
    1 - Adicionado cargos da LAPD

---
+++ atualização.lua
@@ -0,0 +1,40 @@
+[Atualização] : 2/12/2018 As 16:37
+
+1 - Adicionado LOADSCREEN!
+[Atualização] : 2/12/2018 As 16:37
+
+1 - Adicionado ANUNCIOS!
+Gabriel&MilenaÚltimo Domingo às 17:02
+[Atualização] : 2/12/2018 As 17:02
+1 - Adicionado Customchat
+2 - Cinto de segurança
+3 - /dv
+[Atualização] : 2/12/2018 As 17:02
+1 - Adicionado sem mapa!
+Gabriel&MilenaÚltimo Domingo às 17:19
+[Atualização] : 2/12/2018 As 17:02
+1 - Adicionado k9
+[Atualização] : 2/12/2018 As 17:02
+1 - Adicionado fora do rp
+[Atualização] : 2/12/2018 As 17:02
+1 - Adicionado anonimo
+[Atualização] : 2/12/2018 As 17:02
+1 - Adicionado twitter
+Gabriel&MilenaÚltimo Domingo às 17:31
+[Atualização] : 2/12/2018 As 17:31
+1 - Adicionado Menu admin v2.0(editado)
+[Atualização] : 2/12/2018 As 17:31
+1 - Adicionado topo com nome do servidor e discord
+[Atualização] : 2/12/2018 As 17:31
+1 - Adicionado voz no canto direito inferior
+[Atualização] : 2/12/2018 As 17:31
+1 - Adicionado Garage 2.0
+Gabriel&MilenaÚltimo Domingo às 17:41
+[Atualização] : 2/12/2018 As 17:31
+1 - Adicionado correção do celular
+Gabriel&MilenaÚltimo Domingo às 18:26
+[Atualização] : 2/12/2018 As 17:31
+1 - Adicionado nome do servidor
+Gabriel&MilenaÚltimo Domingo às 19:57
+[Atualização] : 2/12/2018 As 19:56
+1 - Adicionado cargos da LAPD
